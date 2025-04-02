dd718f81fd2a7d6ba4185063b3441201229b7a2d
Diana López Alvas <48812037+dlopezalvas@users.noreply.github.com>
2025-04-02 19:03:22 -0300

    Agrego código hecho en clase

--- src/Library.hs
+++ src/Library.hs
@@ -6,3 +6,17 @@ doble numero = numero + numero
 
 siguiente :: Number -> Number
 siguiente nro = nro + 1
+
+costoViaje :: Number -> Number
+costoViaje = precioViaje.minutos.tiempo
+
+tiempo :: Number -> Number
+tiempo distancia = distancia * 1.5 / 100
+
+
+precioViaje :: Number -> Number
+precioViaje minutos = 5000 + 200 * minutos
+
+minutos :: Number -> Number
+minutos horas = horas * 60
+
